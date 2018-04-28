14b51bf7a8ba7b09d7d995fad5420e752bbf591d
valefline <38818143+valefline@users.noreply.github.com>
2018-04-28 17:21:40 -0400

    Update userlogin.php

--- userlogin.php
+++ userlogin.php
@@ -1,8 +1,8 @@
 <html>
     <head>
         <title>Planet Daily - User Login</title>
-        <link rel="stylesheet" href="../views/style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <link rel="stylesheet" href="style.css">
+
     </head>
     <body>
         <div id="wrapper" style="height: 100%;">
